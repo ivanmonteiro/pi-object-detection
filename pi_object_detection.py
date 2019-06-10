15b5a38c84e503cb25c37e548bfdd908418bdba6
Ivan Cantalice <ivanmonteiroc@gmail.com>
2019-06-10 15:15:32 -0300

    changes

--- pi_object_detection.py
+++ pi_object_detection.py
@@ -4,6 +4,7 @@
 # import the necessary packages
 from imutils.video import VideoStream
 from imutils.video import FPS
+from imutils.video import FileVideoStream
 from multiprocessing import Process
 from multiprocessing import Queue
 import numpy as np
@@ -13,41 +14,44 @@ import time
 import cv2
 
 def classify_frame(net, inputQueue, outputQueue):
-	# keep looping
-	while True:
-		# check to see if there is a frame in our input queue
-		if not inputQueue.empty():
-			# grab the frame from the input queue, resize it, and
-			# construct a blob from it
-			frame = inputQueue.get()
-			frame = cv2.resize(frame, (300, 300))
-			blob = cv2.dnn.blobFromImage(frame, 0.007843,
-				(300, 300), 127.5)
+    # keep looping
+    while True:
+        # check to see if there is a frame in our input queue
+        if not inputQueue.empty():
+            # grab the frame from the input queue, resize it, and
+            # construct a blob from it
+            frame = inputQueue.get()
+            frame = cv2.resize(frame, (300, 300))
+            blob = cv2.dnn.blobFromImage(frame, 0.007843,
+                (300, 300), 127.5)
 
-			# set the blob as input to our deep learning object
-			# detector and obtain the detections
-			net.setInput(blob)
-			detections = net.forward()
+            # set the blob as input to our deep learning object
+            # detector and obtain the detections
+            net.setInput(blob)
+            detections = net.forward()
 
-			# write the detections to the output queue
-			outputQueue.put(detections)
+            # write the detections to the output queue
+            outputQueue.put(detections)
 
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-p", "--prototxt", required=True,
-	help="path to Caffe 'deploy' prototxt file")
+    help="path to Caffe 'deploy' prototxt file")
 ap.add_argument("-m", "--model", required=True,
-	help="path to Caffe pre-trained model")
+    help="path to Caffe pre-trained model")
 ap.add_argument("-c", "--confidence", type=float, default=0.2,
-	help="minimum probability to filter weak detections")
+    help="minimum probability to filter weak detections")
 args = vars(ap.parse_args())
 
 # initialize the list of class labels MobileNet SSD was trained to
 # detect, then generate a set of bounding box colors for each class
-CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
-	"bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
-	"dog", "horse", "motorbike", "person", "pottedplant", "sheep",
-	"sofa", "train", "tvmonitor"]
+CLASSES = ["background", "aviao", "bicicleta", "passaro", "barco",
+    "garrafa", "onibus", "carro", "gato", "cadeira", "vaca", "mesa",
+    "cachorro", "cavalo", "moto", "pessoa", "planta", "ovelha",
+    "sofa", "trem", "tv"]
+
+CLASSES_FILTRAR = ["onibus", "carro", "moto", "bicicleta", "pessoa"]
+
 COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
 
 # load our serialized model from disk
@@ -64,76 +68,80 @@ detections = None
 # execution
 print("[INFO] starting process...")
 p = Process(target=classify_frame, args=(net, inputQueue,
-	outputQueue,))
+    outputQueue,))
 p.daemon = True
 p.start()
 
 # initialize the video stream, allow the cammera sensor to warmup,
 # and initialize the FPS counter
 print("[INFO] starting video stream...")
-vs = VideoStream(src=0).start()
-# vs = VideoStream(usePiCamera=True).start()
+#vs = VideoStream(src=0).start()
+#vs = VideoStream(usePiCamera=True).start()
+#video_src = 'sample.mp4'
+video_src = 'ufpe01-60fps.mp4'
+vs = FileVideoStream(video_src).start()
 time.sleep(2.0)
 fps = FPS().start()
 
 # loop over the frames from the video stream
 while True:
-	# grab the frame from the threaded video stream, resize it, and
-	# grab its imensions
-	frame = vs.read()
-	frame = imutils.resize(frame, width=400)
-	(fH, fW) = frame.shape[:2]
+    # grab the frame from the threaded video stream, resize it, and
+    # grab its imensions
+    frame = vs.read()
+    frame = imutils.resize(frame, width=300)
+    (fH, fW) = frame.shape[:2]
 
-	# if the input queue *is* empty, give the current frame to
-	# classify
-	if inputQueue.empty():
-		inputQueue.put(frame)
+    # if the input queue *is* empty, give the current frame to
+    # classify
+    if inputQueue.empty():
+        inputQueue.put(frame)
 
-	# if the output queue *is not* empty, grab the detections
-	if not outputQueue.empty():
-		detections = outputQueue.get()
+    # if the output queue *is not* empty, grab the detections
+    if not outputQueue.empty():
+        detections = outputQueue.get()
 
-	# check to see if our detectios are not None (and if so, we'll
-	# draw the detections on the frame)
-	if detections is not None:
-		# loop over the detections
-		for i in np.arange(0, detections.shape[2]):
-			# extract the confidence (i.e., probability) associated
-			# with the prediction
-			confidence = detections[0, 0, i, 2]
+    # check to see if our detectios are not None (and if so, we'll
+    # draw the detections on the frame)
+    if detections is not None:
+        # loop over the detections
+        for i in np.arange(0, detections.shape[2]):
+            # extract the confidence (i.e., probability) associated
+            # with the prediction
+            confidence = detections[0, 0, i, 2]
 
-			# filter out weak detections by ensuring the `confidence`
-			# is greater than the minimum confidence
-			if confidence < args["confidence"]:
-				continue
+            # filter out weak detections by ensuring the `confidence`
+            # is greater than the minimum confidence
+            if confidence < args["confidence"]:
+                continue
 
-			# otherwise, extract the index of the class label from
-			# the `detections`, then compute the (x, y)-coordinates
-			# of the bounding box for the object
-			idx = int(detections[0, 0, i, 1])
-			dims = np.array([fW, fH, fW, fH])
-			box = detections[0, 0, i, 3:7] * dims
-			(startX, startY, endX, endY) = box.astype("int")
+            # otherwise, extract the index of the class label from
+            # the `detections`, then compute the (x, y)-coordinates
+            # of the bounding box for the object
+            idx = int(detections[0, 0, i, 1])
+            dims = np.array([fW, fH, fW, fH])
+            box = detections[0, 0, i, 3:7] * dims
+            (startX, startY, endX, endY) = box.astype("int")
 
-			# draw the prediction on the frame
-			label = "{}: {:.2f}%".format(CLASSES[idx],
-				confidence * 100)
-			cv2.rectangle(frame, (startX, startY), (endX, endY),
-				COLORS[idx], 2)
-			y = startY - 15 if startY - 15 > 15 else startY + 15
-			cv2.putText(frame, label, (startX, y),
-				cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
+            # draw the prediction on the frame
+            if CLASSES[idx] in CLASSES_FILTRAR:
+                label = "{}: {:.2f}%".format(CLASSES[idx],
+                    confidence * 100)
+                cv2.rectangle(frame, (startX, startY), (endX, endY),
+                    COLORS[idx], 2)
+                y = startY - 15 if startY - 15 > 15 else startY + 15
+                cv2.putText(frame, label, (startX, y),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
 
-	# show the output frame
-	cv2.imshow("Frame", frame)
-	key = cv2.waitKey(1) & 0xFF
+    # show the output frame
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
 
-	# if the `q` key was pressed, break from the loop
-	if key == ord("q"):
-		break
+    # if the `q` key was pressed, break from the loop
+    if key == ord("q"):
+        break
 
-	# update the FPS counter
-	fps.update()
+    # update the FPS counter
+    fps.update()
 
 # stop the timer and display FPS information
 fps.stop()
