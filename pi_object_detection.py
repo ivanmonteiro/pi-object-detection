cded84fe29320ec57d4b6b86b798a117ea317abd
Ivan Monteiro Cantalice <ivanmonteiroc@gmail.com>
2019-06-14 16:28:30 -0300

    cleaning code

--- pi_object_detection.py
+++ pi_object_detection.py
@@ -36,7 +36,7 @@ def classify_frame(net, inputQueue, outputQueue):
             # construct a blob from it
             frame = inputQueue.get()
             #frame = cv2.resize(frame, (NET_INPUT_SIZE, NET_INPUT_SIZE))
-            #frame_resized = imutils.resize(frame, width=NET_INPUT_SIZE)
+            frame_resized = imutils.resize(frame, width=NET_INPUT_SIZE)
             #blob = cv2.dnn.blobFromImage(frame, 0.007843,
             #    (300, 300), 127.5)
             blob = cv2.dnn.blobFromImage(frame, size=(NET_INPUT_SIZE, NET_INPUT_SIZE), swapRB=True, crop=False)
@@ -142,13 +142,13 @@ while True:
         break
 
     frame = imutils.resize(frame, width=300)
-    frame_for_net = cv2.resize(frame, (NET_INPUT_SIZE, NET_INPUT_SIZE))
+    #frame_for_net = cv2.resize(frame, (NET_INPUT_SIZE, NET_INPUT_SIZE))
     (fH, fW) = frame.shape[:2]
 
     # if the input queue *is* empty, give the current frame to
     # classify
     if inputQueue.empty():
-        inputQueue.put(frame_for_net)
+        inputQueue.put(frame)
 
     # if the output queue *is not* empty, grab the detections
     if not outputQueue.empty():
